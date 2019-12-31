3fd8b9cfe85b0601516adfc2768511862844d974
ismail-ru-cse-39 <shaikat.ru.39@gmail.com>
2019-12-31 14:25:33 +0000

    Network class is created with function sigmoid(), feedforward() and SGD()

--- Digit_recognition.py
+++ Digit_recognition.py
@@ -5,3 +5,14 @@ Created on Tue Dec 31 13:56:41 2019
 @author: Ismail
 """
 
+class Network(object):
+    
+    def __init__(self, sizes):
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
+        
+a = [1,2,3,4,5]
+print(a[:-1])
+print(a[1:])
